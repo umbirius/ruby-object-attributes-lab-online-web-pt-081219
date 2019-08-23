b2e22a4b80a1408a707a94e16d80a60a45b19dbf
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-23 15:56:47 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,21 @@
 class Person 
   
-  def name 
-
+  def name=(name)
+    @name = name 
+  end 
+  
+  def name
+    @name
+  end 
+  
+  def job=(job)
+    @job 
+  end 
+  
+  def job 
+    @job
+  end 
+  
+    
 
 end 
