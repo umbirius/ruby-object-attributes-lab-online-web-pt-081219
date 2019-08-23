2f417ac69408cb416f148ab45e98c0397a0085da
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-23 15:53:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,16 @@
 class Dog 
 
-  def initialize(name) 
-   @name = name
+def initialize(name) 
+    @name = name
+   end 
+ 
+  def name=(new_name)
+    @name = new_name
  end 
  
- def name=(new_name)
-   @name = new_name
- end 
- 
+  def name 
+    @name 
+end 
+
+  
 end 
